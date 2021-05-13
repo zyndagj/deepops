e1ed86d47b84b3d72284ee6d97591a7657e4200c
Greg Zynda <zyndagj@gmail.com>
2021-05-13 00:05:03 -0500

    Excluded dashes from vars files

--- virtual/vagrant_startup.sh
+++ virtual/vagrant_startup.sh
@@ -340,8 +340,8 @@ if [ ! -e $CV ]; then
   ed "Linking virtual inventory to config"
   ln -fs ../virtual/inventory ${CV}/inventory
   ed "Injecting vars from deepops/virtual/vars_files"
-  cat vars_files/virt_k8s.yml >> ${CV}/group_vars/k8s-cluster.yml
-  cat vars_files/virt_slurm.yml >> ${CV}/group_vars/slurm-cluster.yml
+  grep -v '\-\-\-\+' vars_files/virt_k8s.yml >> ${CV}/group_vars/k8s-cluster.yml
+  grep -v '\-\-\-\+' vars_files/virt_slurm.yml >> ${CV}/group_vars/slurm-cluster.yml
   ei "You can reset this configuration by deleting deepops/config.virtual and redeploying"
 fi
 [ ! -L ${CV}/inventory ] && ew "Existing config inventory is not linked to the virtual inventory. Please delete and re-run or link the current inventory 'ln -s ../virtual/inventory ../config/inventory'"
